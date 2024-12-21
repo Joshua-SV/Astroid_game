2edc0180916e3c4c242ec50b2cc922ce9119976f
Joshua-SV <J.JSerna@hotmail.com>
2024-12-20 22:14:22 -0600

    added a cap to FPS to 60 to decouple from CPU speeds

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from constants import *
 def main():
     pygame.init() # initialize the pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")  # Optional: Set a window title
+    clock = pygame.time.Clock() # create an object for FPS 
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,255,255))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # ensures a 60 FPS 
 
 if __name__ == "__main__":
     main()
